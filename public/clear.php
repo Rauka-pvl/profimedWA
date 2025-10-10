9617264ce1b72867fb570fa4dca845bdd06acd8d
Rauka-pvl <mr.redle3@gmail.com>
2025-10-10 16:04:44 +0500

    first commit

--- public/clear.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('LARAVEL_START', microtime(true));
-require __DIR__ . '/vendor/autoload.php';
-
-$app = require_once __DIR__ . '/bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->call('optimize:clear');
-echo "✅ Laravel cache cleared successfully!";
