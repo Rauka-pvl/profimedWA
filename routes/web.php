ae6bd5bfb155958b9ff741b7d267d5d02fb5b12d
Rauka-pvl <mr.redle3@gmail.com>
2025-10-13 23:26:22 +0500

    2 commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+Route::get('/php-test', function () {
+    echo "✅ PHP работает<br>";
+
+    $phpVersion = phpversion();
+    echo "Версия PHP: " . $phpVersion . "<br>";
+
+    if (function_exists('opcache_get_status')) {
+        echo "OPcache: включён<br>";
+    } else {
+        echo "OPcache: выключен или недоступен<br>";
+    }
+
+    if (function_exists('curl_init')) {
+        echo "cURL доступен<br>";
+    } else {
+        echo "cURL не установлен<br>";
+    }
+
+    try {
+        $pdo = new PDO('mysql:host=localhost', 'root', '');
+        echo "MySQL доступен<br>";
+    } catch (Exception $e) {
+        echo "MySQL недоступен<br>";
+    }
+
+    echo "<br>Время сервера: " . date('Y-m-d H:i:s');
+});
