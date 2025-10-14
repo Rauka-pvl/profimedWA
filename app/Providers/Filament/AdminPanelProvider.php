a0e8bfe805414f33346e8c0e691f81728d5c1563
Rauka-pvl <mr.redle3@gmail.com>
2025-10-14 08:21:52 +0500

    2 commit

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -60,7 +60,6 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->cacheDisk('local')
             ->unsavedChangesAlerts(false);
     }
 }
